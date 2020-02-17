abdf2dd29ef651f0f02f79aa0f32f93f1dc695cd
Claudio <cvertemara@csumb.edu>
2020-02-16 16:24:16 -0800

    Mario Added

---
+++ Mario/Assets/Scripts/BreakBricks.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Claudio Vertemara
+//CST 326 Mario (Project 2)
+//Feb 16, 2020
+public class BreakBricks : MonoBehaviour
+{
+
+    public Text coinsText;
+    public int coins = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit)) {
+
+                BoxCollider bColl = hit.collider as BoxCollider;
+
+                if (bColl != null) {
+                    Destroy(bColl.gameObject);
+
+                    coins += 5;
+
+                    if (coins < 10) {
+                        coinsText.text = ("x 0" + coins.ToString());
+                    } else {
+                        coinsText.text = ("x " + coins.ToString());
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Mario/Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Claudio Vertemara
+//CST 326 Mario (Project 2)
+//Feb 16, 2020
+
+public class Timer : MonoBehaviour
+{
+    public int startTime = 400;
+
+    TimeSpan startT;
+    TimeSpan nowT;
+    TimeSpan timeDifference;
+
+    public Text timeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startT = new TimeSpan(System.DateTime.Now.Ticks);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nowT = new TimeSpan(System.DateTime.Now.Ticks);
+
+        timeDifference = nowT.Subtract(startT);
+
+        timeText.text = ("TIME\n" + (startTime - (int)timeDifference.TotalSeconds));
+    }
+}
